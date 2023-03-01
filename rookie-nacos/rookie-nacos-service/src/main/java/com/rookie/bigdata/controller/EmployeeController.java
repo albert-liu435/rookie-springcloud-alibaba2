15e9f8035f143354f9851b693e1438548981e6d9
liuxili01 <liuxili@bestseller.com.cn>
2023-03-01 15:48:54 +0800

    nacos

---
+++ rookie-nacos/rookie-nacos-service/src/main/java/com/rookie/bigdata/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.rookie.bigdata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname EmployeeController
+ * @Description
+ * @Author rookie
+ * @Date 2023/3/1 15:47
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+}
